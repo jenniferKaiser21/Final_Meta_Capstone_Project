8ffbde7b730a213676feb9b8f0aeb2d2110f92b0
Jennifer Kaiser <jenniferkaiser.tech@gmail.com>
2023-04-11 17:45:46 -0400

    initial upload of files

---
+++ littlelemon/restaurant/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+# Create your models here.
+
+
+class Booking(models.Model):
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.DecimalField(max_digits=6, decimal_places=0)
+    BookingDate = models.DateTimeField()
+
+
+class Menu:
+    Title = models.CharField(max_length=255)
+    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    Inventory = models.DecimalField(max_digits=5, decimal_places=0)
